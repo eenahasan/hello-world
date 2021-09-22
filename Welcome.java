6706e6d04a3dc6363b5867823da8bbb9380703bc
eenahasan <90691402+eenahasan@users.noreply.github.com>
2021-09-22 11:49:22 +0800

    Create Welcome.java
    Start compile and java code in a GitHub

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+package chap1; 
+
+public class Welcome { 
+  public static void main(String[] args) {
+	    System.out.println("Welcome to Solving problem using \nJava Programming Language");
+ }
+}
